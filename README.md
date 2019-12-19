b09c7a467b97f27479760a665230c3a2fe2f26fa
yangxin <yangxin@htyunwang.com>
2019-12-19 10:08:14 +0800

    renmen

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 1.请到config目录下修改相关配置信息
 
-2.依赖库：selenium,yaml,xlrd,faker,smtplib,email,PIL,pytesseract
+2.依赖库：selenium 
+
+faker：构造随机数据
+
+smtplib,email：发送邮件
+
+PIL,pytesseract：验证码识别
+
+yaml,xlrd:数据处理
+
+retrying：失败重跑
 
 3.配置Tesseract-OCR： https://blog.csdn.net/weixin_44394054/article/details/103362391
 
--- txt.py
+++ txt.py
@@ -1,7 +1,18 @@
-import os
+import random
 
-from config.pathes import USERINFO
-from utils.FileReader import YamlReader
+from retrying import retry
 
 
-print(os.getcwd())
+@retry(stop_max_attempt_number=11)
+class flu():
+    def __init__(self):
+        self.sum = 0
+
+
+    def retry_test(self):
+        a = random.randint(1,10)
+        b = random.randint(1,5)
+        self.sum += 1
+        assert a == b
+        print(self.sum)
+flu().retry_test()
