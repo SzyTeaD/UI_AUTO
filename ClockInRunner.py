abf0ea6d846bd3d16f4aca39111166c851760a09
yangxin <yangxin@htyunwang.com>
2019-12-19 22:38:07 +0800

    优化邮件输出内容

--- ClockInRunner.py
+++ ClockInRunner.py
@@ -1,6 +1,7 @@
-from utils.mail import send_mail
+from utils.mail import  Mail
 from utils.report import runner
 
 test = 'test_oa_clockin*'
 runner(test)
-send_mail()
+eml = Mail()
+eml.send_mail()
