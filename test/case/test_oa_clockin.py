b7c579a205eef3c3ea6bdb14007d5a5d0ecfdbd4
yangxin <yangxin@htyunwang.com>
2019-12-19 16:05:45 +0800

    增加retrying

--- test/case/test_oa_clockin.py
+++ test/case/test_oa_clockin.py
@@ -3,6 +3,7 @@ import time
 import unittest
 
 from appium import webdriver
+from retrying import retry
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
@@ -12,17 +13,18 @@ from test.page.M3OAClockPage import Clock
 from test.page.M3OALoginPage import M3OALogin
 from test.page.M3OANavigationPage import Navigation
 from utils.FileReader import YamlReader
+from utils.log import Logger
 
 
 class ClockIn(unittest.TestCase):
     @classmethod
     def setUpClass(self):
+        self.logger = Logger('OA').get_logger()
         time.sleep(random.randint(1, 30))
         yang = YamlReader(USERINFO).get('OA')[0]
         for i,v in yang.items():
             self.username=i
             self.password=v
-            print(i)
         desired_caps = {}
         desired_caps['platformName'] = 'Android'
         desired_caps['platformVersion'] = '9.1'
@@ -31,22 +33,23 @@ class ClockIn(unittest.TestCase):
         desired_caps['appActivity'] = '.ui.LoadActivity'
         self.dr = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_caps)
 
+    @retry(stop_max_attempt_number=3)
     def test1_login(self):
         time.sleep(3)
         try:
             self.dr.find_element_by_xpath("//android.widget.Button[@text='跳过']")
             self.dr.find_element_by_xpath("//android.widget.Button[@text='跳过']").click()
-            print('跳过势密码设置')
+            self.logger.info('跳过势密码设置')
         except Exception:
-            print('无手势密码设置')
+            self.logger.info('无手势密码设置')
         try:
             self.dr.find_element_by_name("提示")
             self.dr.find_element_by_name("我知道了").click()
             time.sleep(1)
             self.dr.keyevent(4)
-            print('跳过弱密码修改')
+            self.logger.info('跳过弱密码修改')
         except Exception:
-            print('无弱密码修改')
+            self.logger.info('无弱密码修改')
         try:
             Navigation(self.dr).mybtn()
             WebDriverWait(self.dr, 15, 1).until(
@@ -56,26 +59,27 @@ class ClockIn(unittest.TestCase):
             WebDriverWait(self.dr, 15, 1).until(
                 expected_conditions.presence_of_element_located((By.ID,"com.seeyon.cmp:id/buttonDefaultPositive"))).click()
         except Exception:
-            print('已退出登录')
+            self.logger.info('已退出登录')
         M3OALogin(self.dr).uesernametext(self.username)
         M3OALogin(self.dr).pswdtext(self.password)
         M3OALogin(self.dr).surebtn()
         try:
             self.dr.find_element_by_xpath("//android.widget.Button[@text='跳过']")
             self.dr.find_element_by_xpath("//android.widget.Button[@text='跳过']").click()
-            print('跳过势密码设置')
+            self.logger.info('跳过势密码设置')
         except Exception:
-            print('无手势密码设置')
+            self.logger.info('无手势密码设置')
         try:
             self.dr.find_element_by_name("提示")
             self.dr.find_element_by_name("我知道了").click()
             time.sleep(1)
             self.dr.keyevent(4)
-            print('跳过弱密码修改')
+            self.logger.info('跳过弱密码修改')
         except Exception:
-            print('无弱密码修改')
-        print(self.username)
+            self.logger.info('无弱密码修改')
+        self.logger.info('%s已成功登录' % self.username)
 
+    @retry(stop_max_attempt_number=3)
     def test2_seleadress(self):
         Navigation(self.dr).workbenchbtn()
         time.sleep(1)
@@ -87,12 +91,14 @@ class ClockIn(unittest.TestCase):
         except Exception:
             Clock(self.dr).select_address()
         time.sleep(1)
+        self.logger.info('%s已成功定位' % self.username)
 
-    def test4_clockin(self):
+    @retry(stop_max_attempt_number=3)
+    def test3_clockin(self):
         Clock(self.dr).clockbtn()
         Clock(self.dr).clockinbtn()
         Clock(self.dr).attendancebtn()
-        print("打卡成功")
+        self.logger.info('%s已成功打卡' % self.username)
 
 
     @classmethod
